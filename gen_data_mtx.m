6b0167f06c5e62b114db27ecf3385295fe6f09c4
Nicolas Boumal <nicolasboumal@gmail.com>
2017-11-27 17:16:22 -0500

    typo in input name

--- gen_data_mtx.m
+++ gen_data_mtx.m
@@ -1,4 +1,4 @@
-function y_mat = gen_data_mtx(y,W,widnow_shift)
+function y_mat = gen_data_mtx(y,W,window_shift)
 
 % rearranging the data as a matrix
 % input:
@@ -9,11 +9,11 @@ function y_mat = gen_data_mtx(y,W,widnow_shift)
 % y_mat - the data arranged as a matrix. Each column is a windowed signal
 
 N = length(y); y_stretch = [y ; y(1:W-1)];
-Nw = ceil(N/widnow_shift); % need to verfift the ceiling for non-integer fraction 
+Nw = ceil(N/window_shift); % need to verfift the ceiling for non-integer fraction 
 y_mat = zeros(W,Nw);
 
 for i = 0:Nw-1
-    y_mat(:,i+1) = y_stretch(i*widnow_shift+1:i*widnow_shift+W);
+    y_mat(:,i+1) = y_stretch(i*window_shift+1:i*window_shift+W);
 end
 
 end
