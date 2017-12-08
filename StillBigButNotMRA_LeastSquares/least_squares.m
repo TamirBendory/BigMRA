9c2f13e2e7cf34897253c4783b32908789ed6507
Nicolas Boumal <nicolasboumal@gmail.com>
2017-12-08 16:12:45 -0500

    Now optimizing with x of length W instead of L, so that we do not force 0's at the end of the signal: this seems to give more room for escaping local traps.

--- StillBigButNotMRA_LeastSquares/least_squares.m
+++ StillBigButNotMRA_LeastSquares/least_squares.m
@@ -1,7 +1,7 @@
 function x_est = least_squares(M1, M2, M3, W, sigma, N, L, m)
 % Still-Big-But-Not-MRA via least-squares solve. Homogeneous for now.
 
-    manifold = euclideanfactory(L, 1);
+    manifold = euclideanfactory(W, 1); %% !!! W or L: chose wisely -- W seems to help
     
     params.M1 = M1;
     params.M2 = M2;
@@ -15,8 +15,19 @@ function x_est = least_squares(M1, M2, M3, W, sigma, N, L, m)
     
     warning('off', 'manopt:getHessian:approx');
     
-    x_est = trustregions(problem);
-    
+    [x_est, loss] = trustregions(problem); %#ok<ASGLU>
+%     
+%     % Trying to escape local optima...
+%     x_base = x_est;
+%     for shift = 1 : L-1
+%         x_est_shifted = circshift_ad(x_base, shift);
+%         [x_est_shifted, loss_shifted] = trustregions(problem, x_est_shifted);
+%         if loss_shifted < loss
+%             loss = loss_shifted;
+%             x_est = x_est_shifted;
+%         end
+%     end
+%     
     warning('on', 'manopt:getHessian:approx');
     
 end
