e5bae5aab41ad37df8c3ad338c0f73b7d084aa4c
Nicolas Boumal <nicolasboumal@gmail.com>
2017-12-08 16:15:16 -0500

    Now computing M3 symmetrically

--- StillBigButNotMRA_LeastSquares/moments_from_data_no_debias.m
+++ StillBigButNotMRA_LeastSquares/moments_from_data_no_debias.m
@@ -14,9 +14,15 @@ function [M1, M2, M3] = moments_from_data_no_debias(y, W)
 
     % Third order moment
     M3 = zeros(W, W);
-    for k = 0:(W-1)
-        for j = 0:(W-1)
-            M3(k+1, j+1) = sum(y.*circshift_ad(y, k).*circshift_ad(y, j));
+%     for k = 0:(W-1)
+%         for j = 0:(W-1)
+%             M3(k+1, j+1) = sum(y.*circshift_ad(y, k).*circshift_ad(y, -j));
+    % Now computing symmetrically; shouldn't make a difference in amount of
+    % information collected. Requires W odd.
+    assert(W/2 == round(W/2), 'Current code requires W odd.');
+    for k = -(W-1)/2:(W-1)/2
+        for j = -(W-1)/2:(W-1)/2
+            M3(k+(W+1)/2, j+(W+1)/2) = sum(y.*circshift_ad(y, k).*circshift_ad(y, -j));
         end
     end
 
