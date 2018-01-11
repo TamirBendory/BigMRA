0ff4164d7ed846d314c162f83278284ae592ea37
Nicolas Boumal <nicolasboumal@gmail.com>
2018-01-11 17:45:59 -0500

    New file and small changes to allow circular shifts on images, in both directions.

--- StillBigButNotMRA_LeastSquares/circshift_ad.m
+++ StillBigButNotMRA_LeastSquares/circshift_ad.m
@@ -2,8 +2,8 @@ function x = circshift_ad(x, k)
 % Quick implementation of circshift for vectors, because Matlab's circshift
 % is not recognized by ADiMat (for automatic differentiation in Manopt.)
 
-    n = length(x);
+    n = size(x, 1);
     k = mod(k, n);
-    x = x([end-(k-1):end, 1:(end-k)]);
+    x = x([end-(k-1):end, 1:(end-k)], :);
 
 end
