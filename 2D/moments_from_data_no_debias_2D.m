7956befea2ad629d8bdaecddc1bd06257ff69da9
Nicolas Boumal <nicolasboumal@gmail.com>
2018-01-15 18:58:21 -0500

    Using parfor for this too.

--- 2D/moments_from_data_no_debias_2D.m
+++ 2D/moments_from_data_no_debias_2D.m
@@ -5,18 +5,18 @@ function [M1, M2, M3] = moments_from_data_no_debias_2D(Y, list2, list3)
 % Each row of list2 and list3 contains integers (shifts)
 
     % Initializations for AD
-    k1 = 0;
-    k2 = 0;
-    l1 = 0;
-    l2 = 0;
-    shiftedY = zeros(size(Y));
-    shiftedYY = zeros(size(Y));
+%     k1 = 0;
+%     k2 = 0;
+%     l1 = 0;
+%     l2 = 0;
+%     shiftedY = zeros(size(Y));
+%     shiftedYY = zeros(size(Y));
     
     M1 = sum(Y(:));
     
     n2 = size(list2, 1);
     M2 = zeros(n2, 1);
-    for k = 1 : n2
+    parfor k = 1 : n2
         k1 = list2(k, 1);
         k2 = list2(k, 2);
         shiftedY = circshift_ad_2D(Y, [k1, k2]);
@@ -25,7 +25,7 @@ function [M1, M2, M3] = moments_from_data_no_debias_2D(Y, list2, list3)
     
     n3 = size(list3, 1);
     M3 = zeros(n3, 1);
-    for k = 1 : n3
+    parfor k = 1 : n3
         k1 = list3(k, 1);
         k2 = list3(k, 2);
         l1 = list3(k, 3);
