5fc7a52af13a512149217177a58a499f6116593d
Nicolas Boumal <nicolasboumal@gmail.com>
2018-01-16 09:55:00 -0500

    No longer using parfor (will try more things soon), and also changed the places constant multipliers appear to reduce computations (better to multiply a scalar f than a matrix G.)

--- 2D/least_squares_2D_cost_grad.m
+++ 2D/least_squares_2D_cost_grad.m
@@ -31,21 +31,21 @@ function [f, g] = least_squares_2D_cost_grad(X, params)
     % Adjoint of the 2D circular shift operator
     CSadj = @(X, k) circshift(X, -k);
     
-    w1 = 1/m;
-    w2 = 1/(W^2)/m;
-    w3 = 1/(W^4)/m;
+    w1 = 1;
+    w2 = 1/(W^2);
+    w3 = 1/(W^4);
     
     % First-order moment, forward model
     M1 = m*sum(X(:));
     R1 = M1 - M1data;
-    f = f + w1*R1^2;
-    g = g + w1*2*R1*ones(L, L)*m;
+    f = f + .5*w1*R1^2/m;
+    g = g + w1*R1*ones(L, L);
     
     ZPX = ZP(X);
     
     % Second-order moments, forward model
     n2 = size(list2, 1);
-    parfor k = 1 : n2
+    for k = 1 : n2
         
         shift = list2(k, :);
         CX = CS(ZPX, shift);
@@ -53,19 +53,19 @@ function [f, g] = least_squares_2D_cost_grad(X, params)
         
         M2k = M2k + bias2(k);
         
-        R2 = M2k - M2data(k);
-        f = f + w2*R2^2;
+        R2k = M2k - M2data(k);
+        f = f + .5*w2*R2k^2/m;
         
         % remainder for gradient
         CaX = CSadj(ZPX, shift);
         G = ZPadj(CX + CaX);
-        g = g + w2*2*R2*G*m;
+        g = g + w2*R2k*G;
         
     end
     
     % Third-order moments, forward model
     n3 = size(list3, 1);
-    parfor k = 1 : n3
+    for k = 1 : n3
         
         line = list3(k, :);     % intermediate step to help parfor
         shift1 = line([1, 2]);
@@ -77,14 +77,14 @@ function [f, g] = least_squares_2D_cost_grad(X, params)
 
         M3k = M3k + bias3(k);
         
-        R3 = M3k - M3data(k);
-        f = f + w3*R3^2;
+        R3k = M3k - M3data(k);
+        f = f + .5*w3*R3k^2/m;
         
         % remainder for gradient
         T2 = CSadj(CX1 .* ZPX, shift2);
         T3 = CSadj(CX2 .* ZPX, shift1);
         G = ZPadj(T1 + T2 + T3);
-        g = g + w3*2*R3*G*m;
+        g = g + w3*R3k*G;
         
     end
     
