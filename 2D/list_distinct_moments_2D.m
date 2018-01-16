65c9641c125535e5c1cb1e983aa2a4b2c5b53dd0
Nicolas Boumal <nicolasboumal@gmail.com>
2018-01-16 12:14:49 -0500

    Code to check if better to use intersect or ismember // call with [list2, list3, T] = list_distinct_moments_2D(11); mean(T)

--- 2D/list_distinct_moments_2D.m
+++ 2D/list_distinct_moments_2D.m
@@ -1,4 +1,4 @@
-function [list2, list3] = list_distinct_moments_2D(W)
+function [list2, list3, TIMES] = list_distinct_moments_2D(W)
 
     % Code limited to odd W for now.
     assert((W-1)/2 == round((W-1)/2), 'W assumed odd in this code.');
@@ -24,13 +24,27 @@ function [list2, list3] = list_distinct_moments_2D(W)
     % all possible moments and check symmetry rules to exclude most.
     list3 = zeros(W^4, 4);
     k = 0;
+    T = 0;
+    TIMES = zeros(W^4, 3);
     for k1 = (-top) : top
         for k2 = (-top) : top
             for l1 = (-top) : top
                 for l2 = (-top) : top
                     candidate = [k1, k2, l1, l2];
                     equivalents = symmetry_rules_3rd_order_2D(candidate, top);
-                    if isempty(intersect(list3(1:k, :), equivalents, 'rows'))
+                    T = T + 1;
+                    tic;
+                    flag1 = isempty(intersect(list3(1:k, :), equivalents, 'rows'));
+                    TIMES(T, 1) = toc();
+                    tic;
+                    flag2 = sum(ismember(equivalents, list3(1:k, :), 'rows')) == 0;
+                    TIMES(T, 2) = toc();
+                    tic;
+                    flag3 = sum(ismember(list3(1:k, :), equivalents, 'rows')) == 0;
+                    TIMES(T, 3) = toc();
+                    assert(flag1 == flag2);
+                    assert(flag1 == flag3);
+                    if flag1
                         k = k + 1;
                         list3(k, :) = candidate;
                     end
