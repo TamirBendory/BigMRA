0296ac70ec2b9610d0fb4811e94f1004d79f779b
Nicolas Boumal <nicolasboumal@gmail.com>
2018-01-30 13:43:54 -0500

    Cleaned up code and made it not require the signal processing toolbox

--- 2D/align_by_energy_xcorr.m
+++ 2D/align_by_energy_xcorr.m
@@ -1,11 +1,14 @@
-function x_out = align_by_energy_xcorr(x,L)
+function x_out = align_by_energy_xcorr(x, L)
 % input: 
-% x  -  the zero-padded input signal
-% L  -  size of output image
+%    x  -  an image (matrix) of size WxW, with W >= L
+%    L  -  desired output size
+%
 % output: 
-% x_out - L X L subimage of x that contains most of the energy
-
-I = xcorr2(x.^2,ones(L));
-[~,ind] = max(I(:));
-[ind1, ind2] = ind2sub(size(I),ind);
-x_out = x(ind1-L+1:ind1,ind2-L+1:ind2);
+%    x_out - LxL subimage of x that contains most of the energy (in Frobenius norm)
+    
+    I = conv2(x.*conj(x), ones(L))
+    [~, ind] = max(I(:));
+    [ind1, ind2] = ind2sub(size(I), ind);
+    x_out = x(ind1-L+1:ind1, ind2-L+1:ind2);
+    
+end
