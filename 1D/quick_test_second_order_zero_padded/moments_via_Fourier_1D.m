eba0a39a2921db8b00ac2976173586461e9932af
Nicolas Boumal <nicolasboumal@gmail.com>
2018-02-02 14:11:39 -0500

    Generalized test file to show we can use Fourier to get zero-padded autocorrelations with other lengths than L and 2L-1; added timing to check when it's worth it.

--- 1D/quick_test_second_order_zero_padded/moments_via_Fourier_1D.m
+++ 1D/quick_test_second_order_zero_padded/moments_via_Fourier_1D.m
@@ -1,19 +1,25 @@
 % NB: see notes Jan. 31, 2018 (notebook 33)
+clear;
+close all;
+clc;
 
 % This script shows precise relationaships between the moments we compute
 % in BigMRA and monomials in Fourier domain for zero-padded signals.
 
-% Pick a signal length and a true signal (the code works both for real and
-% complex signals.)
-L = 5;
-% x = randn(L, 1);
-x = randn(L, 1) + 1i*randn(L, 1);
+% Pick a micrograph length and a signal length, and a micrograph
+% (the code works both for real and complex data.)
+N = 50;
+L = 50;
+x = randn(N, 1); % + 1i*randn(N, 1);
 
-% Pick a zero-padding length; the only requirement is W >= 2L-1, and we
-% might as well take is as small as possible, so:
-W = 2*L-1;
+tic();
 
-% Zero-pad the signal up to length W, then take its DFT
+% Since we are only interested in autocorrelations with maximum shift
+% separation up to L-1, we only need to zero pad with L-1 elements. If we
+% actually did this, the micrograph would end up with length N+L-1.
+W = N+L-1;
+
+% Implicitly zero-pad the signal up to length W, then take its DFT
 Fxz = fft(x, W);
 
 % Power spectrum of the zero padded signal
@@ -23,24 +29,40 @@ Pxz = real(Fxz).^2 + imag(Fxz).^2; % Fxz .* conj(Fxz);
 M2_Fourier = ifft(Pxz);
 M2_Fourier = M2_Fourier(1:L);
 
+fprintf('M2 Fourier: %.2g [ms]\n', 1000*toc());
+
+tic();
+
 % Do a direct computation of the second moment (s is the shift)
 M2_direct = zeros(L, 1);
 for s = 0 : (L-1)
-    range1 = s : (L-1);
+    range1 = s : (N-1);
     range2 = range1 - s;
     M2_direct(1+s) = sum(x(1+range1) .* conj(x(1+range2)));
 end
 
+fprintf('M2 direct:  %.2g [ms]\n', 1000*toc());
+
 fprintf('Error on 2nd moment: %g\n', norm(M2_direct - M2_Fourier, 2)/norm(M2_direct, 2));
 
+tic();
+
 % Now for third order moments via Fourier: take the usual bispectrum of the
-% zero padded signal
+% zero padded signal: this might not be practical for large N.
 C = circulant(Fxz);
 Bxz = (Fxz*Fxz') .* C;
 
-% Inverse 2D DFT of Bxz, and extract leading LxL submatrix
+% Inverse 2D DFT of Bxz
 M3_Fourier = ifft2(Bxz);
+
+% Extract leading LxL submatrix and zero-out coefficients with separation
+% more than L-1.
 M3_Fourier = M3_Fourier(1:L, 1:L);
+M3_Fourier = transpose(flipud((tril(rot90(M3_Fourier))))); % this has got to have a nicer expression
+
+fprintf('M3 Fourier: %.2g [ms]\n', 1000*toc());
+
+tic();
 
 % Do a direct computation of the third moment (s1, s2 are shifts)
 % Do a direct computation of the second moment (s is the shift)
@@ -48,12 +70,14 @@ M3_direct = zeros(L, L);
 for s1 = 0 : (L-1)
     for s2 = 0 : (L-1)
         if s1 + s2 <= L-1 % other moments are 0
-            range1 = s2 : (L-1-s1);
+            range1 = s2 : (N-1-s1);
             range2 = range1 - s2;
             range3 = range1 + s1;
             M3_direct(1+s1, 1+s2) = sum(x(1+range1) .* conj(x(1+range2)) .* x(1+range3));
         end
     end
 end
+
+fprintf('M3 direct:  %.2g [ms]\n', 1000*toc());
 
 fprintf('Error on 3rd moment: %g\n', norm(M3_direct - M3_Fourier, 'fro')/norm(M3_direct, 'fro'));
