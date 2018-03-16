cc20aaa599ee442dd4fb48475abb127494bb1433
Nicolas Boumal <nicolasboumal@gmail.com>
2018-03-16 14:22:08 -0400

    Par-foor seems to hurt (also, changing N to n)

--- 1D/moments_from_data_no_debias_1D.m
+++ 1D/moments_from_data_no_debias_1D.m
@@ -5,20 +5,20 @@ function [M1, M2, M3] = moments_from_data_no_debias_1D(y, list2, list3)
 % Each row of list2 and list3 contains integers (shifts) -- can be negative
 
     y = y(:);
-    N = size(y, 1);
+    n = size(y, 1);
 
     M1 = sum(y);
     
     n2 = size(list2, 1);
     M2 = zeros(n2, 1);
-    parfor k = 1 : n2
+    for k = 1 : n2
         
         shift1 = list2(k);
         
         vals1 = [0, shift1];
-        range1 = (1+max(vals1)) : (N+min(vals1));
+        range1 = (1+max(vals1)) : (n+min(vals1));
         
-        y1 = y(range1); %#ok<PFBNS>
+        y1 = y(range1);
         y2 = y(range1-shift1);
         
         M2(k) = sum(y1 .* y2);
@@ -27,16 +27,16 @@ function [M1, M2, M3] = moments_from_data_no_debias_1D(y, list2, list3)
     
     n3 = size(list3, 1);
     M3 = zeros(n3, 1);
-    parfor k = 1 : n3
+    for k = 1 : n3
         
         shifts = list3(k, :);
         shift1 = shifts(1);
         shift2 = shifts(2);
 
         vals1 = [0, shift1, shift2];
-        range1 = (1+max(vals1)) : (N+min(vals1));
+        range1 = (1+max(vals1)) : (n+min(vals1));
         
-        y1 = y(range1); %#ok<PFBNS>
+        y1 = y(range1);
         y2 = y(range1-shift1);
         y3 = y(range1-shift2);
         
