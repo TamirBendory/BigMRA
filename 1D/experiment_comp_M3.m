ff17f9076ffcaf11730b8ebd135e46803acd7341
Nicolas Boumal <nicolasboumal@gmail.com>
2018-03-16 14:22:24 -0400

    Really, par-foor doesn't seem to help at all ....

--- 1D/experiment_comp_M3.m
+++ 1D/experiment_comp_M3.m
@@ -1,5 +1,5 @@
 %% Setup some fake long signal to compute moments
-n = 1e8;
+n = 1e9;
 y = randn(n, 1);
 
 shifts = [7, -300];
@@ -25,8 +25,52 @@ tic;
 M3b = 0;
 for k = (1+max(vals1)) : (n+min(vals1))
     M3b = M3b + y(k)*y(k-shift1)*y(k-shift2);
+%     M3b = M3b + prod(y(k-vals1)); % this is very slow
 end
 toc
 
+%% Same in parallel -- this is much slower than a regular for-loop on both
+%  my laptop and on Latte with 72 workers.
+% tic;
+M3c = 0;
+% parfor k = (1+max(vals1)) : (n+min(vals1))
+%     M3c = M3c + y(k)*y(k-shift1)*y(k-shift2);
+% % 
+% %     suby = y([k k k]-vals1);  % failed attempt to avoid communication overhead
+% %     M3c = M3c + suby(1)*suby(2)*suby(3);
+% % 
+% %     M3c = M3c + y(k)*y(k-7)*y(k+300); % it's actually faster (on both laptop and latte) if shifts are fixed, but still comm. overhead and slower overall.
+% end
+% toc
+
+%% Parallel again: trying a different way with dummy variables
+tic;
+M3d = 0;
+y1 = y;
+y2 = y;
+y3 = y;
+% On Latte, for n = 1e7, this is faster than the other parfor, but still
+% slower than a simple loop. For n = 1e8, it's about the same. For n = 1e9,
+% it is slower again. Yeah, so this really doesn't work out...
+parfor k = (1+max(vals1)) : (n+min(vals1))
+    M3d = M3d + y1(k)*y2(k-shift1)*y3(k-shift2);
+end
+toc
+
+%% Again, with different dummies, out of despair: this is indeed slower.
+% tic;
+M3e = 0;
+% y1 = y(range1);
+% y2 = y(range1-shift1);
+% y3 = y(range1-shift2);
+% 
+% parfor k = 1:numel(range1)
+%     M3e = M3e + y1(k)*y2(k)*y3(k);
+% end
+% toc
+
 %%
 fprintf('Relative difference in computed value: %g\n', abs((M3b - M3a) / M3a));
+fprintf('Relative difference in computed value: %g\n', abs((M3c - M3a) / M3a));
+fprintf('Relative difference in computed value: %g\n', abs((M3d - M3a) / M3a));
+fprintf('Relative difference in computed value: %g\n', abs((M3e - M3a) / M3a));
