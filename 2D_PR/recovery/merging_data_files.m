91bfb574a5187634b9882f50ae5ce8a6f9977600
DESKTOP-TCI0O8J\Tamir <tamir.bendory@princeton.edu>
2018-04-13 12:13:34 -0400

    test

--- 2D_PR/recovery/merging_data_files.m
+++ 2D_PR/recovery/merging_data_files.m
@@ -9,6 +9,29 @@ Temp_M3 = M3;
 Temp_m = m_eff(m_eff>0);
 
 load 'data_exp_gpu_second_round';
+Temp_M1 = M1 + Temp_M1;
+Temp_M2 = M2 + Temp_M2;
+Temp_M3 = M3 + Temp_M3;
+m_eff = m_eff(m_eff>0);
+Temp_m = [m_eff; Temp_m];
+
+
+load 'data_exp_gpu_third_round';
+Temp_M1 = M1 + Temp_M1;
+Temp_M2 = M2 + Temp_M2;
+Temp_M3 = M3 + Temp_M3;
+m_eff = m_eff(m_eff>0);
+Temp_m = [m_eff; Temp_m];
+
+load 'data_exp_gpu_fourth_round';
+Temp_M1 = M1 + Temp_M1;
+Temp_M2 = M2 + Temp_M2;
+Temp_M3 = M3 + Temp_M3;
+m_eff = m_eff(m_eff>0);
+Temp_m = [m_eff; Temp_m];
+
+
+load 'data_exp_gpu_fifth_round';
 M1 = M1 + Temp_M1;
 M2 = M2 + Temp_M2;
 M3 = M3 + Temp_M3;
--- 2D_PR/recovery/recovery_rrr.m
+++ 2D_PR/recovery/recovery_rrr.m
@@ -1,9 +1,9 @@
 % Script to run the RRR
 
-%load('data_exp_gpu_first_round.mat');
+load('data_exp_gpu_first_round.mat');
 %load('data_exp_gpu.mat');
 
-merging_data_files;
+%merging_data_files;
 
 fprintf('loading data\n');
 fprintf('sigma = %.2g\n',sigma);
@@ -27,7 +27,8 @@ err_PS = norm(PSX(:) - PS(:))/norm(PSX(:));
 fprintf('error PS = %.6g\n',err_PS);
 
 dc = M1/m_total; % sum(X(:))
-th = .017;
+th = .023;
+
 [Xest_rrr, discrepancy_norm] = RRR(sqrt(PS),L,th);
 figure(43); semilogy(1:length(discrepancy_norm),discrepancy_norm); axis tight;
 %figure(44); plot(1:length(discrepancy_norm),discrepancy_norm); axis tight;
@@ -47,18 +48,22 @@ fprintf('error RRR = %.4g\n',err_rrr);
 %% LS 
 
 N_eff = N*sqrt(n_micrographs);
+if 1
+X0 = zeros(W);
+X0(1:L,1:L) = Xest_rrr;
+opt_window = W;
+max_iter = 200;
+[Xest_LS1, problem,stats] = least_squares_2D(M1, M2, M3, W, sigma, N_eff, L, m_total, list2, list3, X0,'trust_regions',opt_window,max_iter);
 
-%opt_window = W;
-%max_iter = 100;
-%[Xest_LS1, problem,stats] = least_squares_2D(M1, M2, M3, W, sigma, N_eff, L, m_total, list2, list3, Xest_rrr,'trust_regions',opt_window,max_iter);
-
-%Xest_LS1 = Xest_LS1(1:L,1:L);
-%err_LS1 = norm(Xest_LS1(:) - X(:))/norm(X(:));
-%fprintf('error LS1 = %.4g\n',err_LS1);
+Xest_LS1 = Xest_LS1(1:L,1:L);
+err_LS1 = norm(Xest_LS1(:) - X(:))/norm(X(:));
+fprintf('error LS1 = %.4g\n',err_LS1);
+end
 
 opt_window = L;
-max_iter = 500;
-X0 = Xest_rrr;
+max_iter = 1000;
+%X0 = Xest_rrr;
+X0  = Xest_LS1;
 [Xest_LS2, problem,stats] = least_squares_2D(M1, M2, M3, W, sigma, N_eff, L, m_total, list2, list3, X0,'trust_regions',opt_window,max_iter);
 
 err_LS2_2 = norm(Xest_LS2(:) - X(:))/norm(X(:));
@@ -70,38 +75,29 @@ save('RRR_output');
 %% display
 figure(1); colormap gray;
 subplot(131);imagesc(X); axis equal tight off;
-subplot(132);imagesc(Xest_rrr); axis equal tight off;
+subplot(132);imagesc(Xest_LS1); axis equal tight off;
 subplot(133);imagesc(Xest_LS2);axis equal tight off;
 
+%% saving figures 
+ind = 1000:1200;
+
+figure(56);imagesc(Y_clean(ind,ind)); axis image off; colormap gray
+pdf_print_code(gcf, 'data2D_clean.pdf', 11)
+
+sigma = 0.2;
+figure(57); imagesc(sigma*randn(length(ind))+ Y_clean(ind,ind)); axis image off; colormap gray
+pdf_print_code(gcf, 'data2D_noisy_02.pdf', 11)
+
+figure(58); imagesc(Y_obs(ind,ind)); axis image off; colormap gray
+pdf_print_code(gcf, 'data2D_noisy_1.pdf', 11)
+
+figure(59); imagesc(X); axis image off; colormap gray
+pdf_print_code(gcf, 'signal2D_clean.pdf', 11)
+
+figure(60); imagesc(Xest_rrr); axis image off; colormap gray
+pdf_print_code(gcf, 'signal2D_RRR.pdf', 11)
+
+figure(61); imagesc(Xest_LS2); axis image off; colormap gray
+pdf_print_code(gcf, 'signal2D_LS_intermediate.pdf', 11)
 
 
-%% 
-
-% 
-% Mask = zeros(W); 
-% Mask(1:L,1:L) = 1;
-% 
-% 
-% 
-% X0 = Xest_rrr;
-% 
-% num_epocs = 2;
-% err_epoc = zeros(num_epocs,2);
-% obj_epoc = zeros(num_epocs,1);
-% 
-% for i = 1:num_epocs
-% 
-% [X_est, problem,stats] = least_squares_2D(M1, M2, M3, W, sigma, N_eff, L, m_total, list2, list3, X0,'trust_regions');
-% X_est = align_to_reference_2D(X_est, X_zp);
-% fprintf('epoc = %d\n',i);
-% err_epoc(i,1) = norm(X_zp(:) -  X_est(:))/norm(X(:));
-% fprintf('err before proj = %.8g\n',err_epoc(i,1));
-% obj_epoc(i) = stats.cost;
-% X0 = X_est;
-% X0 = X0.*Mask;
-% err_epoc(i,2) = norm(X_zp(:) -  X0(:))/norm(X(:));
-% fprintf('err after proj = %.8g\n',err_epoc(i,2));
-% fprintf('saving\n');
-% save('LS_output');
-% end
-% 
