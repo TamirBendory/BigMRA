7870668375769186f778901365ae2543bdf7c531
Nicolas Boumal <nicolasboumal@gmail.com>
2018-04-19 14:15:48 -0400

    Saving one experiment (with all data) that may go into the paper -- though perhaps we'll want to do repetitions of this to average the RMSE? Not sure, it's easier to interpret this way, and we can state that this is pretty robust, which it is.

---
+++ TEX/XP_1D_homogeneous/example_homogeneous_progressive.m
@@ -0,0 +1,150 @@
+clear;
+close all;
+clc;
+
+%% Pick parameters and generate signal
+
+% Pick one signal of length L and the size W of the separation window.
+L = 21;
+W = 2*L-1;
+% X = randn(L, 1);
+X = zeros(L, 1);
+X(1:7) = 1;
+X(8:11) = -1;
+X(12:L) = .5;
+
+% Pick a noise level.
+sigma = 3;
+
+% Desired length of micrograph.
+n = 1e10;
+
+% Desired number of occurrences of the signal.
+m_want = floor(n/(5*W));
+
+fprintf('Micrograph length: %g\n\n\n', n);
+
+%% Pick which correlation coefficients to sample
+
+[list2, list3] = moment_selection(L, 'exclude biased');
+
+%% Generate the micrograph
+
+[y_clean, m_actual] = generate_clean_micrograph_1D_heterogeneous(X, W, n, m_want);
+y_obs = y_clean + sigma*randn(n, 1);
+gamma = m_actual*L/n;
+SNR = norm(y_clean, 'fro')/norm(y_obs-y_clean, 'fro');
+
+%% The grand experiment starts here
+
+% Select sizes of sub-micrographs to consider.
+ns = unique(round(logspace(5, log10(n), 9)));
+
+% How many times do we optimize from a different random initial guess?
+n_init_optim = 3;
+
+for iter = 1 : length(ns)
+
+    % Collect the moments for the first bit of the micrograph.
+    nn = ns(iter);
+    [M1, M2, M3] = moments_from_data_no_debias_1D_batch( ...
+                                           y_obs(1:nn), list2, list3, 1e8);
+	%! We normalize by nn here.
+    moments.M1 = M1 / nn;
+    moments.M2 = M2 / nn;
+    moments.M3 = M3 / nn;
+    moments.list2 = list2;
+    moments.list3 = list3;
+
+    % Parameters and initializations for optimization:
+    % empty inputs mean default values are picked.
+    L_optim = 2*L-1;
+    sigma_est = [];
+    X0 = [];
+    gamma0 = []; % True value is: m_actual*L_optim/n
+    
+    % Run the optimization from different random initializations n_repeat
+    % times, and keep the best result according to the cost value of X2.
+    result = struct();
+    result.cost_X2 = inf;
+    for repeat = 1 : n_init_optim
+        [X2, gamma2, X1, gamma1, X1_L, cost_X2] = heterogeneous_1D( ...
+                            moments, 1, L, L_optim, sigma_est, X0, gamma0);
+        if cost_X2 < result.cost_X2
+            result.X1 = X1;
+            result.X1_L = X1_L;
+            result.X2 = X2;
+            result.gamma1 = gamma1;
+            result.gamma2 = gamma2;
+            result.RMSE1 = norm(X1_L-X)/norm(X);
+            result.RMSE2 = norm(X2-X)/norm(X);
+            result.cost_X2 = cost_X2;
+        end
+    end
+
+    % Save in a structure array.
+    results(iter) = result; %#ok<SAGROW>
+    
+end
+
+clear y_clean y_obs;
+
+ID = randi(1000000);
+
+save(sprintf('progressive_n%d_%d.mat', n, ID));
+
+%%
+figure(1);
+
+for iter = 1 : length(ns)
+    result = results(iter);
+    subplot(3, 3, iter);
+    T = 0:(L-1);
+    plot(T, result.X1_L, T, result.X2, T, X);
+    title(sprintf('n = %d', ns(iter)));
+    
+    % Hack cosmetics for specific experience
+%     if ns(iter) < 2e6
+%         ylim([-5, 5]);
+%     else
+        ylim([-2, 2]);
+        set(gca, 'YTick', [-2, 0, 2]);
+        set(gca, 'XTick', [0, 10, 20]);
+        set(gca, 'FontSize', 14);
+%     end
+    
+%     hleg = legend(sprintf('First estimate (%.2g)', result.gamma1*L/L_optim), ...
+%                   sprintf('Final estimate (%.2g)', result.gamma2), ...
+%                   sprintf('Ground truth (%.2g)', result.gamma));
+%     set(hleg, 'Location', 'northoutside');
+%     set(hleg, 'Orientation', 'horizontal');
+end
+
+set(gcf, 'Color', 'w');
+
+figname1 = sprintf('progressive_n%d_%d', n, ID);
+savefig(1, [figname1, '.fig']);
+pdf_print_code(1, [figname1 '.pdf'], 14);
+
+%%
+figure(2);
+
+loglog(ns, [results.RMSE1], '.-', ns, [results.RMSE2], '.-');
+hleg = legend('Long estimate', 'Final estimate');
+set(hleg, 'Location', 'northeast');
+set(hleg, 'Orientation', 'vertical');
+set(hleg, 'Box', 'off');
+title('Root mean squared error on signal estimation');
+set(gcf, 'Color', 'w');
+xlabel('Observation length n');
+ylabel('RMSE');
+set(gca, 'FontSize', 14);
+grid on;
+
+figname2 = sprintf('progressive_RMSE_n%d_%d.fig', n, ID);
+savefig(2, [figname2, '.fig']);
+pdf_print_code(2, [figname2 '.pdf'], 14);
+
+P = polyfit(log10(ns), log10([results.RMSE2]), 1);
+fprintf('Slope: %g\n', P(1));
+
