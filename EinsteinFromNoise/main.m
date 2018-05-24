f06a704e7774d2aa7999f55f5b095b7a24cb10d3
DESKTOP-Q2ECRNM\Math User <tamir.bendory@princeton.edu>
2018-05-23 22:32:00 -0400

    the autocorrelation function of cryo-EM

--- EinsteinFromNoise/main.m
+++ EinsteinFromNoise/main.m
@@ -8,22 +8,26 @@ L = 50;
 X = imresize(X, [L, L]);
 
 % number of images
-n = 100000; 
-N = 0.4578*randn(L,L,n);
+n = 1000000; 
 %N = circshift(X,[10,20]);
-N_aligned = zeros(L,L,n);
-
-for i = 1:n
+Xest = zeros(L,L,10);
+for j = 1:10
+ j
+N_aligned = zeros(L,L,n/10);
+N = 0.4578*randn(L,L,n/10);
+for i = 1:n/10
     a = xcorr2(X,N(:,:,i));
     [max_a, imax] = max((a(:)));
     [xpeak, ypeak] = ind2sub(size(a),imax(1));
    % N_aligned(:,:,i) = 
     N_aligned(:,:,i) = circshift(N(:,:,i),[xpeak-L,ypeak-L]);
 end
+Xest(:,:,j) = mean(N_aligned,3);
+end
 
-Xest = mean(N_aligned,3);
+Xest1 = mean(Xest,3);
 
-figure; imagesc(Xest);
+figure; imagesc(Xest1);
 colormap gray; axis equal tight off square
 str = strcat('Einstein_from_noise_n',num2str(n));
 pdf_print_code(gcf, str , 12);
